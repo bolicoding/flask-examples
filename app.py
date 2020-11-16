a4bf9ae3488c05099dda251759c135a35e9610e5
bolicoding <bolicoding@gmail.com>
2020-11-16 11:22:44 -0700

    first part done

--- app.py
+++ app.py
@@ -6,16 +6,6 @@ app = Flask(__name__)
 def home():
   return render_template('index.html', name="Boli")
 
-app.route("/slug/<id>")
-def slug(id):
-  return render_template('slug.html', id=id)
-
-
-@app.route('/loops')
-def loops():
-  return render_template('loops.html', content=['cory', 'zac', 'boli', 'eva', 'dan'])
-
-
 
 if __name__ == "__main__":
   app.run(debug=True)
