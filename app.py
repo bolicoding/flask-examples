5dba3420d1333dbc530e51edc2e3798bad589ce9
bolicoding <bolicoding@gmail.com>
2020-11-16 11:04:57 -0700

    basic html template finished

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "<h1>Hello this is the main page</h1>"
+  return render_template('index.html', name="Boli")
+
+app.route("/slug/<id>")
+def slug(id):
+  return render_template('slug.html', id=id)
 
 
-@app.route('/<name>')
-def user(name):
-  return f"Hello, {name}. How are you?"
+@app.route('/loops')
+def loops():
+  return render_template('loops.html', content=['cory', 'zac', 'boli', 'eva', 'dan'])
 
-
-# @app.route('/admin')
-# def admin():
-#   return redirect(url_for('home'))
-
-
-@app.route('/admin')
-def admin():
-  return redirect(url_for('user', name="boli"))
 
 
 if __name__ == "__main__":
